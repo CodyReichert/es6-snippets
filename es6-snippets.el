9cb88b1b4ed825bd400a724f475e6ac7afcbff50
Cody Reichert <cody@reichertbrothers.com>
2015-05-25 16:00:01 -0500

    initial commit of es6-yasnippets.el. Includes snippets for arrow function and let/const variables. More coming soon

---
+++ es6-snippets.el
@@ -0,0 +1,48 @@
+;;; es6-snippets.el --- Yasnippets for ECMAScript 6
+
+;; Copyright (C) 2015 Cody Reichert
+
+;; Author: Cody Reichert <codyreichert@gmail.com>
+;; Keywords: snippets, javascript, es6
+;; Version: 0.0.1
+;; Package-Requires: ((yasnippet "0.7.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; es6-snippets provides Yasnippets for ECMAScript 6 features and syntax.
+
+;;; Code:
+
+(require 'yasnippet)
+
+
+(defvar es6-snippets-root
+  (file-name-directory (or load-file-name (buffer-file-name))))
+
+;;;###autoload
+(defun es6-snippets-initialize ()
+  "Load es6-snippets."
+  (let ((snippets-dir (expand-file-name "snippets" es6-snippets-root)))
+    (when (boundp 'yas-snippets-dirs)
+      (add-to-list 'yas-snippets-dirs snippets-dir t))
+    (yas/load-directory snippets-dir)))
+
+;;;###autoload
+(eval-after-load 'yasnippet
+  '(es6-snippets-initialize))
+
+(provide 'es6-snippets)
+;;; es6-snippets.el ends here
