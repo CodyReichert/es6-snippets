578a2cab209b9ac2121b201d038776a240765512
Cody Reichert <cody@reichertbrothers.com>
2018-07-07 13:51:08 -0500

    Update package commentary
    For melpa release

--- es6-snippets.el
+++ es6-snippets.el
@@ -1,29 +1,30 @@
 ;;; es6-snippets.el --- Yasnippets for ECMAScript 6
-
+;;
 ;; Copyright (C) 2015 Cody Reichert
-
+;;
 ;; Author: Cody Reichert <codyreichert@gmail.com>
-;; Keywords: snippets, javascript, es6
+;; URL: http://github.com/CodyReichert/es6-snippets
+;; Keywords: convenience snippets javascript es6
 ;; Version: 0.0.1
 ;; Package-Requires: ((yasnippet "0.7.0"))
-
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+;;
 ;;; Commentary:
-
+;;
 ;; es6-snippets provides Yasnippets for ECMAScript 6 features and syntax.
-
+;;
 ;;; Code:
 
 (defvar es6-snippets-root (file-name-directory (or load-file-name
